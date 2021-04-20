d188cb3ca8703ee1484ccbb2e0d7331c3eae7e0b
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-21 05:28:58 +0900

    programmers #카펫

---
+++ programmers/level_2/카펫.py
@@ -0,0 +1,9 @@
+﻿def solution(brown, yellow):
+    def max_div(num):
+        max_num = 1
+        for i in range(1, int(num ** (1 / 2) + 1)):
+            if num % i == 0 and ((i + 2) + (num // i)) * 2 == brown:
+                max_num = i
+        return [max_num, num // max_num]
+
+    return [i + 2 for i in sorted(max_div(yellow), reverse=True)]
