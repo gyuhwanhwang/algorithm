6e5245334522d3b422eddc2d27c85fac95f5fa29
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-23 01:46:03 +0900

    programmers #여행경로 #dfs/bfs + 반복 구조

--- programmers/level_3/여행경로.py
+++ programmers/level_3/여행경로.py
@@ -15,3 +15,16 @@ def solution(tickets):
 
     dfs("ICN")
     return path[::-1]
+
+def solution(tickets):
+    graph = defaultdict(list)
+    for a, b in sorted(tickets, reverse=True):
+        graph[a].append(b)
+
+    stack, path = ["ICN"], []
+    while stack:
+        while graph[stack[-1]]:
+            stack.append(graph[stack[-1]].pop())
+        # 막히는 부분에서 풀어냄
+        path.append(stack.pop())
+    return path[::-1]
