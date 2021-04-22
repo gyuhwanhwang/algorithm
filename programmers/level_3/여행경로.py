ceb7d91dee4efdb6100ccebe0b59e42610651aa3
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-22 23:16:53 +0900

    programmers #여행경로 #dfs/bfs

---
+++ programmers/level_3/여행경로.py
@@ -0,0 +1,17 @@
+﻿from collections import defaultdict
+
+
+def solution(tickets):
+    graph = defaultdict(list)
+    for a, b in sorted(tickets, reverse=True):
+        graph[a].append(b)
+
+    path = []
+
+    def dfs(node):
+        while graph[node]:
+            dfs(graph[node].pop())
+        path.append(node)
+
+    dfs("ICN")
+    return path[::-1]
