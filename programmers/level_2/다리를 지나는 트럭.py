053780608ea249859360530d1761c811fc63ae02
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-23 15:41:30 +0900

    programmers #다리를 지나는 트럭 #스택/큐

---
+++ programmers/level_2/다리를 지나는 트럭.py
@@ -0,0 +1,22 @@
+﻿from collections import deque
+
+
+def solution(bridge_length, weight, truck_weights):
+    on_bridge = deque()
+    truck = deque(truck_weights)
+
+    time = 0
+    while on_bridge or truck:
+        time += 1
+        if on_bridge:
+            for i in range(len(on_bridge)):
+                on_bridge[i][1] += 1
+            if on_bridge[0][1] > bridge_length:
+                cur_weight = on_bridge.popleft()[0]
+                weight += cur_weight
+
+        if truck and truck[0] <= weight:
+            on_bridge.append([truck.popleft(), 1])
+            weight -= on_bridge[-1][0]
+
+    return time
