48dd426a7998b5c0081dd408bdc017eebb018ed9
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-25 01:29:25 +0900

    leetcode #1704. Determine if String Halves Are Alike

---
+++ LeetCode/lc_1704. Determine if String Halves Are Alike.py
@@ -0,0 +1,6 @@
+﻿import re
+class Solution:
+    def halvesAreAlike(self, s: str) -> bool:
+        first = s[:len(s)//2].lower()
+        second = s[len(s)//2:].lower()
+        return len(re.findall("[aeiou]", first)) == len(re.findall("[aeiou]", second))
