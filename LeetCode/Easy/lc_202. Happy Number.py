2c1ab70f9b964e8213ffe7d29afe7d9d7110fb63
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-25 04:09:21 +0900

    leetcode #202. Happy Number

---
+++ LeetCode/Easy/lc_202. Happy Number.py
@@ -0,0 +1,28 @@
+﻿from collections import defaultdict
+
+
+class Solution:
+    def isHappy(self, n: int) -> bool:
+        done = set()
+        while n not in done:
+            done.add(n)
+            n = sum(i * i for i in map(int, str(n)))
+        return n == 1
+
+#         visited = defaultdict(int)
+#         result = []
+
+#         def happy(num):
+#             if num == 1:
+#                 result.append(True)
+#                 return
+#             visited[num] += 1
+#             if visited[num] == 2:
+#                 result.append(False)
+#                 return
+#             val = sum([int(str(num)[i]) ** 2 for i in range(len(str(num)))])
+#             # print(val, result)
+#             happy(val)
+
+#         happy(n)
+#         return result[0]
