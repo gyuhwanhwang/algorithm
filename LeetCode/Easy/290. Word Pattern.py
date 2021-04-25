394c2b898a7ac4e35a1098a95f6f92bb482864e8
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-26 02:07:01 +0900

    leetcode #290. Word Pattern

---
+++ LeetCode/Easy/290. Word Pattern.py
@@ -0,0 +1,15 @@
+﻿from collections import defaultdict
+class Solution:
+    def wordPattern(self, pattern: str, s: str) -> bool:
+        table = defaultdict(str)
+        s = s.split()
+        if len(pattern) != len(s):
+            return False
+        for i in range(len(pattern)):
+            if not table[pattern[i]]:
+                if s[i] in table.values():
+                    return False
+                table[pattern[i]] = s[i]
+            elif table[pattern[i]] != s[i]:
+                return False
+        return True
