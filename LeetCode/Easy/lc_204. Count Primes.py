bcbdf769a567ec589c80d15c13caa505674ca1d3
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-25 15:05:43 +0900

    leetcode #204. Count Primes

---
+++ LeetCode/Easy/lc_204. Count Primes.py
@@ -0,0 +1,10 @@
+﻿class Solution:
+    def countPrimes(self, n: int) -> int:
+        answer = [True] * n
+        for i in range(2, int(n ** 0.5) + 1):
+            if answer[i]:
+                for j in range(i + i, n, i):
+                    answer[j] = False
+
+        # return len([i for i in range(2, n) if answer[i] == True])
+        return sum(answer[2:n])
