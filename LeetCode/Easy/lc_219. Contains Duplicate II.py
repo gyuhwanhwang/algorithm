523bbb3b49f76e1d8e9a232cf7d4c7e5b5e819a0
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-26 01:51:02 +0900

    leetcode #219. Contains Duplicate II

---
+++ LeetCode/Easy/lc_219. Contains Duplicate II.py
@@ -0,0 +1,12 @@
+﻿from collections import defaultdict
+class Solution:
+    def containsNearbyDuplicate(self, nums: List[int], k: int) -> bool:
+        i_dict = defaultdict(list)
+        for i in range(len(nums)):
+            if not i_dict[nums[i]]:
+                i_dict[nums[i]].append(i)
+            elif i - i_dict[nums[i]][0] <= k:
+                return True
+            else:
+                i_dict[nums[i]][0] = i
+        return False
