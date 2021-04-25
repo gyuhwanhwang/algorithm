1d0d167f7ed4ab24562efacaf69835e791a4ee1c
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-25 17:05:50 +0900

    leetcode #205. Isomorphic Strings

---
+++ LeetCode/Easy/lc_205. Isomorphic Strings.py
@@ -0,0 +1,16 @@
+﻿from collections import defaultdict
+class Solution:
+    def isIsomorphic(self, s: str, t: str) -> bool:
+        table = defaultdict(str)
+        visited = set()
+        for i in range(len(s)):
+            # 처음 변환하는 단어
+            if not table[s[i]]:
+                # 그런데 값이 이미 다른 키와 매핑되있으면 중복
+                if t[i] in table.values():
+                    return False
+                table[s[i]] = t[i]
+            # 변환한 단어일 경우 변환 값이 맞는지 확인
+            elif table[s[i]] != t[i]:
+                return False
+        return True
