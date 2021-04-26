49205857cfea634d9d9555c1d56fbd5a96c150e6
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-27 01:24:12 +0900

    leetcode #387. First Unique Character in a String

---
+++ LeetCode/Easy/lc_387. First Unique Character in a String.py
@@ -0,0 +1,7 @@
+﻿from collections import Counter
+class Solution:
+    def firstUniqChar(self, s: str) -> int:
+        for k, v in Counter(s).items():
+            if v == 1:
+                return s.index(k)
+        return -1
