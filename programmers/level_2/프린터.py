80ddc2a266739d4fa984cf3626bff80c5c2de715
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-29 03:07:53 +0900

    programmers #프린터 #stack/queue

---
+++ programmers/level_2/프린터.py
@@ -0,0 +1,14 @@
+﻿from collections import deque
+def solution(priorities, location):
+    Q = deque(enumerate(priorities))
+    count = 0
+    while Q:
+        lo, prior = Q.popleft()
+        # if not Q or prior >= max(Q, key = lambda x: x[1])[1]:
+        # if prior >= max(Q, key = lambda x: x[1], default=(-1, 0))[1]:
+        if any(prior < q[1] for q in Q):
+            Q.append((lo, prior))
+        else:
+            count += 1
+            if lo == location:
+                return count
