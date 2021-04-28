e259efa5ec62b2f5b4fca91b137502e657f81fdf
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-28 21:57:31 +0900

    leetcode #389. Find the Difference

---
+++ LeetCode/Easy/lc_389. Find the Difference.py
@@ -0,0 +1,10 @@
+﻿from collections import Counter
+
+
+class Solution:
+    def findTheDifference(self, s: str, t: str) -> str:
+        s_dict, t_dict = Counter(s), Counter(t)
+
+        for key, value in t_dict.items():
+            if value != s_dict[key]:
+                return key389. Find the Difference389. Find the Difference
