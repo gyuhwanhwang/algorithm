04594d3e55b945814c616eb504e6ea9ba4882879
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-28 22:49:43 +0900

    leetcode #409. Longest Palindrome

---
+++ LeetCode/Easy/lc_409. Longest Palindrome.py
@@ -0,0 +1,9 @@
+﻿from collections import Counter
+class Solution:
+    def longestPalindrome(self, s: str) -> int:
+        odd = sum(val & 1 for key, val in Counter(s).items())
+        return len(s) - odd + 1 if odd > 1 else len(s)
+
+    def longestPalindrome(self, s):
+        odds = sum(v & 1 for v in collections.Counter(s).values())
+        return len(s) - odds + bool(odds)
