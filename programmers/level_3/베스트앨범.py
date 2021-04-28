1eedf51d3baf4a2ba1e25848508f58e31be205b8
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-29 01:32:29 +0900

    programmers #베스트앨범 #hash

---
+++ programmers/level_3/베스트앨범.py
@@ -0,0 +1,17 @@
+﻿from collections import defaultdict
+
+
+def solution(genres, plays):
+    def order(a):
+        return sum(i[0] for i in hash[a])
+
+    result = []
+    hash = defaultdict(list)
+    for genre, play, i in sorted(zip(genres, plays, range(len(plays))), key=lambda x: (x[1], -x[2])):
+        hash[genre].append((play, i))
+
+    for key in sorted(hash.keys(), key=order, reverse=True):
+        for _ in range(2):
+            if hash[key]:
+                result.append(hash[key].pop()[1])
+    return result
