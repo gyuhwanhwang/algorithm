3150103afdcd5fabb081f9977f97c5c3870bb031
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-29 17:27:55 +0900

    programmers #더 맵게 #heap

---
+++ programmers/level_2/더 맵게.py
@@ -0,0 +1,11 @@
+﻿import heapq
+
+
+def solution(scoville, K):
+    heapq.heapify(scoville)
+    count = 0
+    while scoville[0] < K and len(scoville) >= 2:
+        count += 1
+        heapq.heappush(scoville, heapq.heappop(scoville) + (heapq.heappop(scoville) * 2))
+
+    return count if scoville[0] >= K else -1
