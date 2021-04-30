a4d861257ff6b939465ea121fb4de600044928ec
gyuhwanhwang <rbghks0327@gmail.com>
2021-04-30 16:37:32 +0900

    programmers #체육복 #greedy

--- programmers/level_1/체육복.py
+++ programmers/level_1/체육복.py
@@ -15,3 +15,29 @@ def solution(n, lost, reserve):
             answer += 1
             reserve[reserve.index(l+1)] = -1
     return answer
+
+def solution(n, lost, reserve):
+    while reserve:
+        num = reserve.pop()
+        if num in lost:
+            lost.remove(num)
+            continue
+        if num-1 not in reserve and num - 1 in lost:
+            lost.remove(num-1)
+            continue
+        if num+1 not in reserve and num + 1 in lost:
+            lost.remove(num+1)
+            continue
+    return n - len(lost)
+
+def solution(n, lost, reserve):
+    _reserve = [r for r in reserve if r not in lost]
+    _lost = [l for l in lost if l not in reserve]
+    for r in _reserve:
+        f = r - 1
+        b = r + 1
+        if f in _lost:
+            _lost.remove(f)
+        elif b in _lost:
+            _lost.remove(b)
+    return n - len(_lost)
