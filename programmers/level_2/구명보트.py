68c38e53b91eec4d8f730bdae7b7775e68df5c65
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-02 18:23:26 +0900

    programmers #구명보트 #greedy

---
+++ programmers/level_2/구명보트.py
@@ -0,0 +1,25 @@
+﻿def solution(people, limit):
+    people.sort()
+    left, right = 0, len(people) - 1
+    count = 0
+    while left < right:
+        while right >= 1 and people[left] + people[right] > limit:
+            right -= 1
+        if left < right:
+            count += 1
+            people[left], people[right] = 0, 0
+            left, right = left + 1, right - 1
+    return count + sum(bool(p) for p in people)
+
+def solution(people, limit) :
+    answer = 0
+    people.sort()
+
+    a = 0
+    b = len(people) - 1
+    while a < b :
+        if people[b] + people[a] <= limit :
+            a += 1
+            answer += 1
+        b -= 1
+    return len(people) - answer
