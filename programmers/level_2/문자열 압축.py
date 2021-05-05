04e8555de87a914015a05c6032ee668fd1115a65
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-05 14:30:51 +0900

    programmers #문자열 압축 #string

---
+++ programmers/level_2/문자열 압축.py
@@ -0,0 +1,18 @@
+﻿def solution(s):
+    string = ""
+    count = 1
+    min_len = len(s)
+    for cut in range(1, len(s)//2 + 1):
+        for i in range(0, len(s) - cut, cut):
+            # s[0:2] == s[2:4] ...
+            if s[i : i+cut] == s[i+cut : i+ 2*cut]:
+                count += 1
+            else:
+                string += str(count) + s[i : i+cut] if count > 1 else s[i : i+cut]
+                count = 1
+            if i >= len(s) - 2*cut: # 마지막 비교
+                string += str(count) + s[i+cut : i+ 2*cut] if count > 1 else s[i+cut : i+ 2*cut]
+                count = 1
+        min_len = min(min_len, len(string))
+        string = ""
+    return min_len
