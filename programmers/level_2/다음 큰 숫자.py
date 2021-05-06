949062f88d745f8590c691d118eedd6df636cbe1
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-07 06:49:38 +0900

    programmers #다음 큰 숫자 #built-in function

---
+++ programmers/level_2/다음 큰 숫자.py
@@ -0,0 +1,14 @@
+﻿def solution(n):
+    num_of_1 = bin(n).count('1')
+    for num in range(n + 1, 1000001):
+        if num_of_1 == bin(num).count('1'):
+            return num
+
+    #     def ones(num):
+    #         return sum(True for i in bin(num)[2:] if i == '1')
+
+    #     n1 = ones(n)
+    #     while True:
+    #         n += 1
+    #         if n1 == ones(n):
+    #             return n
