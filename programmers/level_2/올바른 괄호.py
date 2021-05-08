4240fd53947c30238e50557a66c9f69585c96a7b
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-09 02:31:34 +0900

    #programmers #올바른 괄호 #stack/queue

---
+++ programmers/level_2/올바른 괄호.py
@@ -0,0 +1,23 @@
+﻿def solution(s):
+    stack = []
+    table = {
+        ')': '(',
+    }
+
+    for char in s:
+        if char not in table:
+            stack.append(char)
+        elif not stack or table[char] != stack.pop():
+            return False
+
+    return not stack
+
+    stack = []
+    for i in s:
+        if i == '(':
+            stack.append(i)
+        else:
+            if len(stack) == 0:
+                return False
+            stack.pop()
+    return not stack
