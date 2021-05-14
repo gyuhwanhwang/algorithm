3d8df9713bdac34fba08434a80a80bbc94971b64
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-14 20:08:58 +0900

    #programmers #캐시 #stack/queue

---
+++ programmers/level_2/캐시.py
@@ -0,0 +1,34 @@
+﻿from collections import deque
+def solution(cacheSize, cities):
+    cache = deque()
+    time = 0
+    for city in [city.lower() for city in cities]:
+        if city in cache:
+            time += 1
+            cache.remove(city)
+            cache.append(city)
+        elif len(cache) < cacheSize:
+            cache.append(city)
+            time += 5
+        elif cache:
+            cache.popleft()
+            cache.append(city)
+            time += 5
+        else:
+            time += 5
+    return time
+
+def solution(cacheSize, cities):
+    import collections
+    cache = collections.deque(maxlen=cacheSize)
+    time = 0
+    for i in cities:
+        s = i.lower()
+        if s in cache:
+            cache.remove(s)
+            cache.append(s)
+            time += 1
+        else:
+            cache.append(s)
+            time += 5
+    return time
