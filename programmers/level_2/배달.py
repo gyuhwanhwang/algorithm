507d2aceb974e1ee2840bd1b890143d6f0791131
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-18 02:32:41 +0900

    #programmers #배달 #graph

---
+++ programmers/level_2/배달.py
@@ -0,0 +1,20 @@
+﻿from collections import defaultdict
+import heapq as hq
+def solution(N, road, K):
+    graph = defaultdict(list)
+    for start, end, time in road:
+        graph[start].append((time, end))
+        graph[end].append((time, start))
+
+    dist = defaultdict(int)
+    Q = [(0, 1)]
+    answer = 0
+    while Q:
+        time, node = hq.heappop(Q)
+        if time > K: continue
+        if node not in dist:
+            dist[node] = time
+            answer += 1
+            for next_time, next_node in graph[node]:
+                hq.heappush(Q, (time + next_time, next_node))
+    return answer
