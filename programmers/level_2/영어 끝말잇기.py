bb0680a56e5fba765f87c33513121dc948931a55
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-17 23:51:45 +0900

    #programmers #영어 끝말잇기 #hash

---
+++ programmers/level_2/영어 끝말잇기.py
@@ -0,0 +1,12 @@
+﻿def solution(n, words):
+    answer = [0, 0]
+    prev = set()
+    prev.add(words[0])
+    prev_str = words[0][-1]
+    for i in range(1, len(words)):
+        if words[i] not in prev and words[i][0] == prev_str:
+            prev.add(words[i])
+            prev_str = words[i][-1]
+        else:
+            return [i%n + 1, i//n + 1]
+    return answer
