a27e25c231850970a2002972653b38391e4532ba
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-19 15:13:49 +0900

    #programmers #파일명 정렬 #sort

---
+++ programmers/level_2/파일명 정렬.py
@@ -0,0 +1,7 @@
+﻿import re
+def solution(files):
+    files.sort(key=lambda x: (re.findall('[a-zA-Z.\- ]+', x)[0].lower(), int(re.findall('[0-9]+', x)[0])))
+    return files
+
+solution(["img12.png", "img10.png", "img02.png", "img1.png", "IMG01.GIF", "img2.JPG"])
+solution(["ba af3.png", "cd-ef10.png", "aa.a02.png", "aab1.png", "a-z01.GIF", "A2.JPG"])
