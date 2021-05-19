0d0aa3fdddffec44877ea111acd775d15e8445e9
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-19 17:08:12 +0900

    #programmers #압축 #hash

---
+++ programmers/level_2/압축.py
@@ -0,0 +1,16 @@
+﻿def solution(msg):
+    dict = {}
+    for i in range(26):
+        dict[chr(ord('A') + i)] = i+1
+    i, left = 27, 0
+    answer = []
+    while left < len(msg):
+        k = 0
+        while left+1+k <= len(msg) and msg[left:left+1+k] in dict:
+            k += 1
+        answer.append(dict[msg[left:left+1+k - 1]])
+        dict[msg[left:left+1+k]] = i
+        i += 1
+        left += k
+
+    return answer
