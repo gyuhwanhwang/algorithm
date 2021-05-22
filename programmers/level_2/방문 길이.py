1f9ecbfc7042543a6ef8e5153224285091f85710
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-23 03:44:00 +0900

    #programmers #방문길이 #hash

---
+++ programmers/level_2/방문 길이.py
@@ -0,0 +1,12 @@
+﻿def solution(dirs):
+    d = { 'U': [0, 1], 'D': [0, -1], 'L': [-1, 0], 'R': [1, 0] }
+    visited = set()
+    x, y = 0, 0
+    for dir in dirs:
+        nx = x + d[dir][0]
+        ny = y + d[dir][1]
+        if not (-5 <= nx <= 5) or not (-5 <= ny <= 5):
+            continue
+        visited.add((max(x,nx), min(x, nx), max(y, ny), min(y,ny)))
+        x, y = nx, ny
+    return len(visited)
