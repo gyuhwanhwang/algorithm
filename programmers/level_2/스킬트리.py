b7da912ed8e69b964dad3abaf39bcc7465e41298
gyuhwanhwang <rbghks0327@gmail.com>
2021-05-25 05:12:46 +0900

    #programmers #스킬트리 #구현

---
+++ programmers/level_2/스킬트리.py
@@ -0,0 +1,18 @@
+﻿def solution(skill, skill_trees):
+    answer = 0
+    for tree in skill_trees:
+        cur = 0
+        for i in range(len(tree) + 1):
+            if i == len(tree):
+                answer += 1
+                break
+            if tree[i] not in skill:
+                continue
+            elif tree[i] == skill[cur]:
+                cur += 1
+            else:
+                break
+    return answer
+
+
+solution("CBD", ["BACDE", "CBADF", "AECB", "BDA"])
